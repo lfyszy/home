60b2f02c150b8191ac62051012ff3d6d8955e385
songziyu <114333178+lfyszy@users.noreply.github.com>
2024-09-04 16:58:51 +0800

    push

--- src/views/Func/index.vue
+++ src/views/Func/index.vue
@@ -1,6 +1,13 @@
 <template>
   <!-- 功能区域 -->
   <div :class="store.mobileFuncState ? 'function mobile' : 'function'">
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <div class="left">
+          <!-- <Hitokoto /> -->
+          <Music v-if="playerHasId" />
+        </div>
+      </el-col>
       <el-col :span="12">
         <div class="right cards">
           <div class="time">
@@ -18,13 +25,6 @@
         </div>
       </el-col>
     </el-row>
-    <el-row :gutter="20">
-      <el-col :span="12">
-        <div class="left">
-          <!-- <Hitokoto /> -->
-          <Music v-if="playerHasId" />
-        </div>
-      </el-col>
   </div>
 </template>
 
