9dd24a08b039b5ef0f4a30f2b5dbc224028ad264
songziyu <114333178+lfyszy@users.noreply.github.com>
2024-09-04 16:43:39 +0800

    fixed

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="more-content">Yeah, I can't do these long drives sitting in the back seat.<\br>
-Thinking'bout that one night when the world caught fire.<\br>
-Baby, and your eyes brightened up this dark scene.<\br>
+  <div class="more-content">Yeah, I can't do these long drives sitting in the back seat.<br>
+Thinking'bout that one night when the world caught fire.<br>
+Baby, and your eyes brightened up this dark scene.<br>
 You told me I would be fine, I called you a liar.</div>
 </template>
 
