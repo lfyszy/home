9809bc8199d9f1f1cbc4f8cef8225c35fee89300
songziyu <114333178+lfyszy@users.noreply.github.com>
2024-09-04 16:17:36 +0800

    push

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -34,13 +34,12 @@ const emit = defineEmits(["loadComplete"]);
 
 // 壁纸随机数
 // 请依据文件夹内的图片个数修改 Math.random() 后面的第一个数字
-// const bgRandom = Math.floor(Math.random() * 1 + 1);
-const bgRandom = 1;
+const bgRandom = Math.floor(Math.random() * 10 + 1);
 
 // 更换壁纸链接
 const changeBg = (type) => {
   if (type == 0) {
-    bgUrl.value = `/images/background${bgRandom}.png`;
+    bgUrl.value = `/images/background${bgRandom}.jpg`;
   } else if (type == 1) {
     bgUrl.value = "https://api.dujin.org/bing/1920.php";
   } else if (type == 2) {
@@ -77,7 +76,7 @@ const imgLoadError = () => {
       fill: "#efefef",
     }),
   });
-  bgUrl.value = `/images/background${bgRandom}.png`;
+  bgUrl.value = `/images/background${bgRandom}.jpg`;
 };
 
 // 监听壁纸切换
